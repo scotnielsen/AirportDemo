aec66daeba825ed6eb18a768159c3a4759343d94
scotnielsen <scotnielsen@yahoo.co.uk>
2017-10-02 16:54:44 +0100

    Build and clean scripts

---
+++ build.bat
@@ -0,0 +1,21 @@
+setlocal
+
+call clean.bat
+
+set COBCPY=%COBCPY%;.\copybooks
+
+
+rem compile library and main executable
+cbllink -d -Udirs.dir -obin\airportlinklibrary.dll AirportLinkLibrary\aircode.cbl
+cbllink -Udirs.dir -obin\AirportConsoleApplication.exe AirportConsoleApplication\airport.cbl
+
+
+rem compile tests
+cbllink -d -Udirs.dir -obin\AirportTests.dll TestAirportLinkLibrary\TestAIRCODE.cbl TestAirportLinkLibrary\TestGetDetails.cbl TestAirportLinkLibrary\TestGetMatches.cbl
+
+copy TestAirportLinkLibrary\TestAirportLinkLibrary.dat .\bin
+set dd_airports=TestAirportLinkLibrary.dat
+mfurun AirportTests.dll
+
+endlocal
+
