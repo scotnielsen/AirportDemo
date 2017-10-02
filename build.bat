d43fee926247e6d43002fd5b50dbabfd83c894ab
scotnielsen <scotnielsen@yahoo.co.uk>
2017-10-02 17:14:04 +0100

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 setlocal
 
-call clean.bat
+call "C:\Program Files (x86)\Micro Focus\Visual COBOL\CreateEnv.bat"
 
 set COBCPY=%COBCPY%;.\copybooks
 
@@ -11,11 +11,11 @@ cbllink -Udirs.dir -obin\AirportConsoleApplication.exe AirportConsoleApplication
 
 
 rem compile tests
-cbllink -d -Udirs.dir -obin\AirportTests.dll TestAirportLinkLibrary\TestAIRCODE.cbl TestAirportLinkLibrary\TestGetDetails.cbl TestAirportLinkLibrary\TestGetMatches.cbl
+; cbllink -d -Udirs.dir -obin\AirportTests.dll TestAirportLinkLibrary\TestAIRCODE.cbl ; TestAirportLinkLibrary\TestGetDetails.cbl TestAirportLinkLibrary\TestGetMatches.cbl
 
-copy TestAirportLinkLibrary\TestAirportLinkLibrary.dat .\bin
-set dd_airports=TestAirportLinkLibrary.dat
-mfurun AirportTests.dll
+; copy TestAirportLinkLibrary\TestAirportLinkLibrary.dat .\bin
+; set dd_airports=TestAirportLinkLibrary.dat
+; mfurun AirportTests.dll
 
 endlocal
 
