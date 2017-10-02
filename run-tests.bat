d2418f1e45633c8800eb4a07438adb73bcc0b138
scotnielsen <scotnielsen@yahoo.co.uk>
2017-10-02 19:09:52 +0100

    Add files via upload

--- run-tests.bat
+++ run-tests.bat
@@ -3,8 +3,9 @@ echo on
 pushd %CD%
 call "C:\Program Files (x86)\Micro Focus\Visual COBOL\CreateEnv.bat"
 popd
+cd bin
 set dd_airports=TestAirportLinkLibrary.dat
-mfurun AirportTests.dll
+mfurun .\bin\AirportTests.dll
 
 endlocal
 
