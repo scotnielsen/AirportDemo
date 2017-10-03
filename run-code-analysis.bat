a5427f93469d59a750e063b712f25ff1e297f86b
scotnielsen <scotnielsenuk@gmail.com>
2017-10-03 16:44:59 +0100

    setup mf env

--- run-code-analysis.bat
+++ run-code-analysis.bat
@@ -1,7 +1,9 @@
 setlocal 
 echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsmsbuildcmd.bat"
-
+pushd %CD%
+call "C:\Program Files (x86)\Micro Focus\Visual COBOL\CreateEnv.bat"
+popd
 msbuild AirportLib\AirportLib.cblproj /p:RunMicrofocusCodeAnalysisAfterBuild=true /p:ActiveRulesets="Within Entire Program;Coding Standards"
 
 endlocal
