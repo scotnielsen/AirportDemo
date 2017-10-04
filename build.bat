89eca87fecbd5db80d84ef77e6f12ed373e283bf
scotnielsen <scotnielsenuk@gmail.com>
2017-10-04 10:42:51 +0100

    correcting build

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 setlocal 
 echo on
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsmsbuildcmd.bat"
+pushd %CD%
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+popd
 msbuild AirportLib\AirportLib.cblproj /t:rebuild
 msbuild AirportApp\AirportApp.cblproj /t:rebuild
+
 endlocal
 
 
