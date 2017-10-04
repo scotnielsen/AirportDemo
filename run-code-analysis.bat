f592f176fda024c55a37949aa86fc8095ac1a87c
scotnielsen <scotnielsenuk@gmail.com>
2017-10-04 11:00:07 +0100

    Capture CA log

--- run-code-analysis.bat
+++ run-code-analysis.bat
@@ -1,5 +1,7 @@
-setlocal 
+setlocal
+ 
 echo on
+
 pushd %CD%
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
@@ -7,7 +9,8 @@ popd
 
 cd airportlib
 
-msbuild AirportLib.cblproj /p:RunMicroFocusCodeAnalysisAfterBuild=True /p:ActiveRuleSets="Within Entire Program; Coding Standards"
+msbuild AirportLib.cblproj /fileLogger /p:RunMicroFocusCodeAnalysisAfterBuild=True /p:ActiveRuleSets="Within Entire Program; Coding Standards"
+find "MFCA" AirportLib\msbuild.log > .\AirportLib\MFCAResults.log
 
 endlocal
 
