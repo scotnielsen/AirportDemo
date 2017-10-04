e542f90dd83b5fb69bdd10e3fa672a81fd03bfdc
scotnielsen <scotnielsenuk@gmail.com>
2017-10-04 13:17:18 +0100

    ...

--- run-code-analysis.bat
+++ run-code-analysis.bat
@@ -7,10 +7,10 @@ cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Too
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
 popd
 
-cd airportlib
-
+CD AirportLib
 msbuild AirportLib.cblproj /fileLogger /p:RunMicroFocusCodeAnalysisAfterBuild=True /p:ActiveRuleSets="Within Entire Program; Coding Standards"
-find "MFCA" AirportLib\msbuild.log > .\AirportLib\MFCAResults.log
+find "MFCA" msbuild.log > MFCAResults.log
+
 
 endlocal
 
